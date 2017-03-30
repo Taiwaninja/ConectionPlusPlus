6cc47309d98482053f1d95b7cba50d38c551be15
whoamiblat123 <whoamiblat123@gmail.com>
2017-03-30 21:15:44 +0300

    fixed spaces

--- GetActivties.py
+++ GetActivties.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python
 
 from bottle import route, run, debug, template, request, static_file, error
-
 import json
 
 def jsonify(dic):
-    print 'dic:',dic
+    """JSON ==> str, pretty-print"""
     return json.dumps(dic, indent=4)
     
-
 @route('/api/helloBlat', methods=["GET"])
 def helloBlat():
     return jsonify({"Blat": "6"})
-
 
 @route("/api/get_mock", methods=["GET"])
 def get_mock():
@@ -21,14 +18,11 @@ def get_mock():
         moses = json.load(mosesFile)
     return jsonify(moses)
 
-
-
 def main():
     # remember to remove reloader=True and debug(True) when you move your
     # application from development to a productive environment
     debug(True)
     run(reloader=True)
-
     
 if __name__ == '__main__':
     main()
