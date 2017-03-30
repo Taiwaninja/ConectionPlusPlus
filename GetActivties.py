af46cc69ac79c4473798d8cf5ba0ac644a32dab0
crispy <crispy>
2017-03-31 00:51:05 +0300

    Added cross origin support and some google changes

--- GetActivties.py
+++ GetActivties.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from bottle import route, run, debug, template, request, static_file, error
+from bottle import route, run, debug, response, template, request, static_file, error, hook
 from Apis.Amadeus.AmadeusClient import AmadeusClient
 import json
 import os
@@ -9,6 +9,17 @@ import os
 def jsonify(dic):
     """JSON ==> str, pretty-print"""
     return json.dumps(dic, indent=4)
+
+
+@hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 
 @route('/api/helloBlat', methods=["GET"])
